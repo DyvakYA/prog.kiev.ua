9348a01c20abfdd63538348ac049637a11784286
Dyvak Yurii <dyvakyurii@gmail.com>
2017-11-19 17:18:05 +0200

    Add files via upload

---
+++ lesson-2/json1/Main.java
@@ -0,0 +1,14 @@
+package lesson2.json1;
+
+import java.io.IOException;
+
+/**
+ * Created by User on 11/19/2017.
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        JsonParser jsonParser = new JsonParser();
+        jsonParser.parse();
+    }
+
+}
